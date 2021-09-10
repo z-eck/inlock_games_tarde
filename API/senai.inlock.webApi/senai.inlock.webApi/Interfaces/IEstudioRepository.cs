c60c69244b895878665b33ba3dbbeadff2d53502
Zeck <61870100+z-eck@users.noreply.github.com>
2021-09-10 14:16:45 -0300

    Add files via upload

---
+++ API/senai.inlock.webApi/senai.inlock.webApi/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,14 @@
+﻿using senai.inlock.webApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    interface IEstudioRepository
+    {
+        List<EstudioDomain> ListarTodos();
+        EstudioDomain BuscarPorID(int idEstudioBuscado);
+    }
+}
---
+++ API/senai.inlock.webApi/senai.inlock.webApi/Interfaces/IJogoRepository.cs
@@ -0,0 +1,17 @@
+﻿using senai.inlock.webApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    interface IJogoRepository
+    {
+        List<JogoDomain> ListarTodos();
+        JogoDomain BuscarPorID(int idJogoBuscado);
+        void Cadastrar(JogoDomain novoJogo);
+        void Atualizar(JogoDomain JogoAtualizado, int idJogoAtualizado);
+        void Deletar(int idJogoDeletado);
+    }
+}
