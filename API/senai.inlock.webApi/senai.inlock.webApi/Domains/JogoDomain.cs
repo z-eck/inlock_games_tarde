ee060d53fb829d34d40de872c8bc1ad0ae050484
Zeck <61870100+z-eck@users.noreply.github.com>
2021-09-13 12:38:57 -0300

    Add files via upload

--- API/senai.inlock.webApi/senai.inlock.webApi/Domains/JogoDomain.cs
+++ API/senai.inlock.webApi/senai.inlock.webApi/Domains/JogoDomain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,17 @@ namespace senai.inlock.webApi.Domain
     {
         public int idJogo { get; set; }
         public int idEstudio { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do Jogo!")]
         public string nomeJogo { get; set; }
+
+        [Required(ErrorMessage = "Insira uma descrição ao Jogo!")]
         public string descricao { get; set; }
+
+        [Required(ErrorMessage = "Insira o Valor do Jogo!")]
         public decimal valor { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime dataLancamento { get; set; }
         EstudioDomain estudio { get; set; }
     }
